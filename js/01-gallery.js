410d2458826dca3d626486089a5af9c9fec894db
Olya Vorontsova <16olya1996@gmail.com>
2023-09-03 15:55:03 +0200

    update task 1
    create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+container.insertAdjacentHTML("beforeend", markup);
+container.addEventListener("click", handleClick);
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`}).join("")
+}
+
